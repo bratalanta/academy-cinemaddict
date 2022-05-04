0c60c68700c8057db04d6e426162f920506d0529
Matvei <mateodenisous69@gmail.com>
2022-05-04 13:18:56 +0300

    Заполяем шаблон рыбой

--- src/main.js
+++ src/main.js
@@ -1,18 +1,39 @@
+import FilmsModel from './model/films-model.js';
 import FilmsPresenter from './presenter/films-presenter.js';
 import { render } from './render.js';
 import FilterView from './view/filter-view.js';
-import PopupView from './view/popup-view.js';
+import FilmPopupView from './view/film-popup-view.js';
 import UserRankView from './view/user-rank-view.js';
 
 const appBodyElement = document.querySelector('body');
 const appMainElement = document.querySelector('.main');
 const appHeaderElement = document.querySelector('.header');
-const filmPresenter = new FilmsPresenter();
+const filmsPresenter = new FilmsPresenter();
+const filmsModel = new FilmsModel();
 
 render(new UserRankView(), appHeaderElement);
 render(new FilterView(), appMainElement);
 
-filmPresenter.init(appMainElement);
+filmsPresenter.init(appMainElement, filmsModel);
 
-render(new PopupView(), appBodyElement);
+const filmsListContainerElement = document.querySelector('.films-list__container');
+
+filmsListContainerElement.addEventListener('click', (evt) => {
+  const {target} = evt;
+
+  if (!target.matches('button')) {
+    const filmCardId = target.closest('.film-card').id;
+
+    for (const film of filmsModel.getFilms()) {
+      if (film.id === filmCardId) {
+        const comments = filmsModel.getComments()
+          .filter(({id}) => film.comments.includes(id));
+
+        render(new FilmPopupView(film, comments), appBodyElement);
+
+        break;
+      }
+    }
+  }
+});
 
---
+++ src/mock/comment.js
@@ -0,0 +1,14 @@
+import { getRandomInteger } from '../utils.js';
+
+const MIN_ID = 0;
+const MAX_ID = 10;
+
+export const generateComment = () => (
+  {
+    id: `${getRandomInteger(MIN_ID, MAX_ID)}`,
+    author: 'Ilya O\'Reilly',
+    comment: 'a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.',
+    date: '2019-05-11T16:12:32.554Z',
+    emotion: 'smile'
+  }
+);
